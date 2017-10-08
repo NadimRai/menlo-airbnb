283f63ffb88276186448a624feea88a568b786fa
NadimRai <rai_nadim@yahoo.com>
2017-10-08 20:46:37 +0800

    generated Rooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
  
 
+  get 'rooms/index'
+
+  get 'rooms/new'
+
+  get 'rooms/create'
+
+  get 'rooms/listing'
+
+  get 'rooms/pricing'
+
+  get 'rooms/description'
+
+  get 'rooms/photo_upload'
+
+  get 'rooms/amenities'
+
+  get 'rooms/location'
+
+  get 'rooms/update'
+
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
