a988b14803662b898c4bf50987259e0ca48421b7
NadimRai <rai_nadim@yahoo.com>
2017-10-15 16:54:52 +0800

    completed hostreviews controller

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,9 +1,40 @@
 class HostReviewsController < ApplicationController
 
 def create
-	@host_review = current_user.host_reviews.create(host_review_params)
+
+	# Step 1: Check if the reservation exist (room_id, guest_id, host_id)
+
+    # Step 2: Check if the current host already reviewed the guest in this reservation.
+
+    @reservation = Reservation.where(
+                    id: host_review_params[:reservation_id],
+                    room_id: host_review_params[:room_id],
+                    user_id: host_review_params[:guest_id]
+                   ).first
+
+    if !@reservation.nil?
+
+    	@has_reviewed = HostReview.where(
+                        reservation_id: @reservation.id,
+                        guest_id: host_review_params[:guest_id]
+                      ).first
+
+	      if @has_reviewed.nil?
+	          # Allow to review
+	          @host_review = current_user.host_reviews.create(host_review_params)
+	          flash[:success] = "Review created..."
+	    else
+	    	 # Already reviewed
+	          flash[:success] = "You already reviewed this Reservation"
+
+	    end
+	else
+		lash[:alert] = "Not found this reservation"
+    end
+
 	redirect_back(fallback_location: request.referer)
 end
+
 
 def destroy
 	@host_review = Review.find(params[:id])
