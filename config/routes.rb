f5fbe6a32aa0222802278c442cdad91cfc544181
NadimRai <rai_nadim@yahoo.com>
2017-10-18 22:44:08 +0800

    route for search

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
+
+  get 'search', to: "pages#search"
   
   root 'pages#home'
 
